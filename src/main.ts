75be5d8c8e0dacebdbdc45354a3384929ffeb200
sanjikasehgal <sanjika.sehgal@hotmail.com>
2023-03-27 07:35:34 -0400

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,3 @@
-// Template
-
-// import { createApp } from 'vue'
-// import App from './App-template.vue'
-// import router from './router-template'
-
-// import './assets-template/main.css'
-
-// const app = createApp(App)
-
-// app.use(router)
-
-// app.mount('#app')
-
-// My mess :) 
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
